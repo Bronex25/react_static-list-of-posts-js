9eedb67e290366c27b536eecd217bd1a587ef74c
Bronex25 <oleksandr.plaksin25@gmail.com>
2024-12-20 15:18:12 +0100

    solution

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,11 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+
+export const PostList = ({ posts }) => (
+  <>
+    <h1 className="App__title">Static list of posts</h1>
+
+    {posts.map(post => (
+      <PostInfo key={post.id} post={post} />
+    ))}
+  </>
+);
